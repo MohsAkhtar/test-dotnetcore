53ad444514b8e5ed15466afa7afc725f2a0aed16
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-15 22:42:02 +0000

    Added logger to project

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -3,24 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace DutchTreat.Data
 {
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _context;
+        private readonly ILogger<DutchRepository> _logger
+            ;
 
-        public DutchRepository(DutchContext context)
+        public DutchRepository(DutchContext context, ILogger<DutchRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         // gets list of all products
         public IEnumerable<Product> GetAllProducts()
-        {
-            return _context.Products
-                .OrderBy(p => p.Title)
-                .ToList();
+        {     
+            try
+            {
+                _logger.LogInformation("GetAllProducts was called");
+                return _context.Products
+                    .OrderBy(p => p.Title)
+                    .ToList();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Failed to get all products: {e}");
+                return null;
+            }
+            
         }
 
 
